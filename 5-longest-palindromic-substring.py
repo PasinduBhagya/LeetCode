6b665a9359604d6e1426f39b618ae6f74a80a586
PasinduBhagya <pasindubhagyapeiries@gmail.com>
2024-08-12 14:24:20 +0530

    5 Half completed - Time Exceeding issue

--- 5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring.py
@@ -1,13 +1,29 @@
+from statistics import median
+
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         stringList = list(s)
-        i = 0
-        while i < len(stringList):
-            letterComp = stringList[i]
+        currentLongestLength = 0
+        k = 0
+        while k < len(stringList):
+            checkingString1 = stringList[k:]
+            for i in range(len(checkingString1)):
+                checkingString = checkingString1[:i+1]
+                if len(checkingString) >  currentLongestLength:
+                    checkingStringReverse = list(reversed(checkingString))
+                    if checkingString == checkingStringReverse:
+                        LogestString = checkingString
+                        print(checkingString)
+                        currentLongestLength = len(LogestString)
+
+            k += 1
+        # return ''.join(LogestString)
+        
+                
             
     
 
 solution = Solution()
 
-s = "abcad"
-solution.longestPalindrome(s)
+s = "vvgogaewginhopuxzwyryobjjpieyhwfopiowxmyylvcgsnhfcnogpqpukzmnpewavoutbloyrrhatimmxfqmcwgfebuoqbbgvubbkjfvxivjfijlpvtsnhagzfptahhyojwzamayoiegkenycnkxzkambimhdykdcxyyfjnvztzypmfczdhhnkmfuvgkhzbwmjznykkagqdrueohgcmeidjqsvfugcioeduohprjtfdmtzosnhoiganffarokxjifzzxhixdzycwfheqqegduzwtiacmdhqfmxhazcxsqyrvrihfqzjxvawdeandnwgjlquvzadruiqmcsgibglhicsvzqknztqpkiihdoisxipkourentfvrltieihiktgzswtgcmmlfrjifqinhrbplbsgswqlbjkyxjwoshsvxlhlpgzwbmxzwaemtowcxwourjwmmwjruowxcwotmeawzxmbwzgplhlxvshsowjxykjblqwsgsblpbrhniqfijrflmmcgtwszgtkihieitlrvftneruokpixsiodhiikpqtznkqzvscihlgbigscmqiurdazvuqljgwndnaedwavxjzqfhirvryqsxczahxmfqhdmcaitwzudgeqqehfwcyzdxihxzzfijxkoraffnagiohnsoztmdftjrphoudeoicgufvsqjdiemcghoeurdqgakkynzjmwbzhkgvufmknhhdzcfmpyztzvnjfyyxcdkydhmibmakzxkncynekgeioyamazwjoyhhatpfzgahnstvpljifjvixvfjkbbuvgbbqoubefgwcmqfxmmitahrryolbtuovawepnmzkupqpgoncfhnsgcvlyymxwoipofwhyeipjjboyrywzxupohnigweagogvv"
+print(solution.longestPalindrome(s))
